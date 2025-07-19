bb854df647ed0d5b23c3ddc8284e18acf4127956
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-07-19 17:09:29 -0400

    Update init.cmake

--- cmake/Hunter/init.cmake
+++ cmake/Hunter/init.cmake
@@ -2,13 +2,13 @@
 # Copyright 2018 The evmone Authors.
 # SPDX-License-Identifier: Apache-2.0
 
-set(HUNTER_CONFIGURATION_TYPES Release CACHE STRING "Build type of Hunter packages")
+set(HUNTER_CONFIGURATION_TYPES Release CACHE STRING "Build type of VX packages")
 set(HUNTER_USE_CACHE_SERVERS NO CACHE STRING "Download binaries from Hunter cache servers")
 
 include(HunterGate)
 
 HunterGate(
-    URL "https://github.com/cpp-pm/hunter/archive/v0.25.3.tar.gz"
-    SHA1 "0dfbc2cb5c4cf7e83533733bdfd2125ff96680cb"
+    URL "https://github.com/cpp-pm/VX/archive/v0.25.3.tar.gz"
+    SHA1 ""
     LOCAL
 )
